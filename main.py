e025fd005aeb260515da917bc56dcd5dbc668a7d
lle1ya <AlexTV2@yandex.ru>
2024-05-29 02:56:00 +0500

    Rework

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 import pygame
+from random import randrange
 
-BackColor = (188, 143, 143)
-Wsite = (255, 235, 205)
-Gsite = (192, 192, 192)
+RES = 800
+SIZE = 50
 
-size = [590, 600]
-SIZE_BLOCK = 20
-COUNT_BLOCKS = 27
-Margin = 1
+x, y = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+length = 1
+snake = [(x, y)]
+dx, dy = 0, 0
+fps = 5
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Змейка')
+pygame.init()
+sc = pygame.display.set_mode([RES, RES])
+clock = pygame.time.Clock()
 
 while True:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-    screen.fill(BackColor)
-
-    for row in range(COUNT_BLOCKS):
-        for column in range(COUNT_BLOCKS):
-            if (row + column) % 2 == 0:
-                color = Wsite
-            else:
-                color = Gsite
-            pygame.draw.rect(screen,color, [10 + column * SIZE_BLOCK + Margin*(column+1),
-                                                 20 + row * SIZE_BLOCK + Margin*(row+1),SIZE_BLOCK,SIZE_BLOCK])
-
-    pygame.display.flip()
+    sc.fill(pygame.color('Black'))
