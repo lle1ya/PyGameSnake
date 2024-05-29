66b21d05f2101fe8c70ad23d20997d39bd221171
lle1ya <AlexTV2@yandex.ru>
2024-05-29 14:12:17 +0500

    Rework

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from ctypes.wintypes import SIZE
+
 import pygame
 from random import randrange
 
@@ -6,6 +8,9 @@ SIZE = 50
 
 x, y = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
 apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+
+pygame.display.set_caption('Змейка')
+
 length = 1
 snake = [(x, y)]
 dx, dy = 0, 0
@@ -16,4 +21,32 @@ sc = pygame.display.set_mode([RES, RES])
 clock = pygame.time.Clock()
 
 while True:
-    sc.fill(pygame.color('Black'))
+    sc.fill(pygame.Color('black'))
+
+    # Рисование змейки и яблока
+    [(pygame.draw.rect(sc, pygame.Color('green'), (i,j, SIZE, SIZE))) for i, j in snake]
+    pygame.draw.rect(sc, pygame.Color('red'), (*apple, SIZE, SIZE))
+
+    # Движение змейки
+    x += dx * SIZE
+    y += dy * SIZE
+    snake.append((x, y))
+    snake = snake[-length:]
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    # Контроль
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        dx, dy = 0, -1
+    if key[pygame.K_s]:
+        dx, dy = 0, 1
+    if key[pygame.K_a]:
+        dx, dy = -1, 0
+    if key[pygame.K_d]:
+        dx, dy = 1, 0
