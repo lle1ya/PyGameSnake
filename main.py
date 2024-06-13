0f2357842c2384bac3f9b0d10ecfa0d084464f46
lle1ya <AlexTV2@yandex.ru>
2024-06-13 23:50:41 +0500

    Добавил сетку на поле

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init()
 screen_width = 800
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Тест')
+pygame.display.set_caption('Змейка')
 
 # Цвета
 white = (255, 255, 255)
